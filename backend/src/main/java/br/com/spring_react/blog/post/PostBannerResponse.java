2de65d9d686a89a30187e5b8d70a57ec9ef27b75
Inaros Lemuel <infromke@gmail.com>
2026-02-01 02:06:34 -0300

    feat: create DTO records to standardize responses in image file endpoints for User and Post modules

---
+++ backend/src/main/java/br/com/spring_react/blog/post/PostBannerResponse.java
@@ -0,0 +1,4 @@
+package br.com.spring_react.blog.post;
+
+public record PostBannerResponse(String message, String banner) {
+}
---
+++ backend/src/main/java/br/com/spring_react/blog/user/UserAvatarResponse.java
@@ -0,0 +1,4 @@
+package br.com.spring_react.blog.user;
+
+public record UserAvatarResponse(String message, String avatar) {
+}
